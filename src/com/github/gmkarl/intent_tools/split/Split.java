a59a5ca49fe6dbe766b4b1015e46bbe2935d727c
example <example@example.com>
2014-09-29 18:47:42 +0000

    Project creation

---
+++ src/com/github/gmkarl/intent_tools/split/Split.java
@@ -0,0 +1,15 @@
+package com.github.gmkarl.intent_tools.split;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Split extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
