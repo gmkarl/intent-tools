d540819888c7d6a64ce8dbeacf486e8c7884282e
example <example@example.com>
2014-09-29 21:18:27 +0000

    Added stream2text project

---
+++ stream2text/src/com/github/gmkarl/intent_tools/stream2text/Stream2Text.java
@@ -0,0 +1,15 @@
+package com.github.gmkarl.intent_tools.stream2text;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Stream2Text extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
