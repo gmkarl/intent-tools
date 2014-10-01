8391c749fcfec5991eb1d9f3b1005e1dee032d07
gmkarl <gmkarl@gmail.com>
2014-10-01 15:25:42 +0000

    Current status

--- split/src/com/github/gmkarl/intent_tools/split/Split.java
+++ split/src/com/github/gmkarl/intent_tools/split/Split.java
@@ -2,6 +2,8 @@ package com.github.gmkarl.intent_tools.split;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.net.Uri;
 
 public class Split extends Activity
 {
@@ -10,6 +12,33 @@ public class Split extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+	
+	Intent intent = getIntent();
+	String action = intent.getAction();
+	String type = intent.getType();
+
+	if (Intent.ACTION_SEND.equals(action) && type != null) {
+		if ("text/plain".equals(type)) {
+			String text = intent.getStringExtra(Intent.EXTRA_TEXT);
+			int length = 858;
+			int offset = text.length() - (text.length() % length);
+			/* * 15: 17 * 17: 32 * 19: 53 * 21: 78 * 23: 106 * 25: 134
+ * 27: 154 * 29: 192 * 31: 230 * 33: 271 * 35: 321 * 37: 367 * 39: 425 * 41: 458 * 43: 520
+ * 45: 586 * 47: 644 * 49: 718 * 51: 792 * 53: 858 * 55: 929 * 57: 1003 * 59: 1091 * 61: 1171 * 63: 1273
+ * 65: 1367 * 67: 1465 * 69: 1528 * 71: 1628 * 73: 1732 * 75: 1840 * 77: 1952 */
+			if (offset < text.length())
+				send(text.substring(offset) );
+			while ( (offset-=length) >= 0 )
+				send(text.substring(offset, offset + length));
+		}
+	}
     }
+	private void send(String text)
+	{
+		Intent sendIntent = new Intent();
+		sendIntent.setAction(Intent.ACTION_SEND);
+		sendIntent.putExtra(Intent.EXTRA_TEXT, text);
+		sendIntent.setType("text/plain");
+		startActivity(sendIntent);
+	}
 }
--- stream2text/src/com/github/gmkarl/intent_tools/stream2text/Stream2Text.java
+++ stream2text/src/com/github/gmkarl/intent_tools/stream2text/Stream2Text.java
@@ -2,6 +2,13 @@ package com.github.gmkarl.intent_tools.stream2text;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.content.Intent;
+import android.net.Uri;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.FileNotFoundException;
 
 public class Stream2Text extends Activity
 {
@@ -10,6 +17,36 @@ public class Stream2Text extends Activity
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+	
+	Intent intent = getIntent();
+	String action = intent.getAction();
+	Uri uri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
+
+	if (Intent.ACTION_SEND.equals(action) && uri != null) {
+
+		InputStream iStream;
+		try {
+			iStream = getContentResolver().openInputStream(uri);
+		} catch (FileNotFoundException e) {
+			return;
+		}
+		BufferedReader reader = new BufferedReader(new InputStreamReader(iStream));
+
+		StringBuilder stringBuilder = new StringBuilder();
+
+		try { for (;;) {
+			String line = reader.readLine();
+			if (line == null) break;
+			stringBuilder.append(line);
+			stringBuilder.append('\n');
+		} } catch (IOException e) { }
+
+		String text = intent.getStringExtra(Intent.EXTRA_TEXT);
+		Intent sendIntent = new Intent();
+		sendIntent.setAction(Intent.ACTION_SEND);
+		sendIntent.putExtra(Intent.EXTRA_TEXT, stringBuilder.toString());
+		sendIntent.setType("text/plain");
+		startActivity(sendIntent);
+	}
     }
 }
